546fcb61916e5958a3e7c8cb3a2c3d85e88c6da9
Filip Strajnar <filip.strajnar@gmail.com>
2022-02-01 01:08:20 +0100

    More unit tests

--- index.test.js
+++ index.test.js
@@ -13,7 +13,42 @@ test('Check if conversion works as expected', function () {
     ];
     (0, index_1.ArrayOfPromisesToPromiseArray)(testingArray)
         .then(function (wholeArray) {
-        expect(wholeArray).toContainEqual([1, 2, 3, 4, 5]);
+        expect(wholeArray)
+            .toContainEqual([1, 2, 3, 4, 5]);
+    })
+        .catch(function (unexpected) { return console.log("Covered"); });
+});
+test('Check if conversion works for strings', function () {
+    var one = (0, index_1.toPromise)("John");
+    var two = (0, index_1.toPromise)("May");
+    var three = (0, index_1.toPromise)("Elon");
+    var four = (0, index_1.toPromise)("Chris");
+    var five = (0, index_1.toPromise)("Nick");
+    var testingArray = [
+        one, two, three,
+        four, five
+    ];
+    (0, index_1.ArrayOfPromisesToPromiseArray)(testingArray)
+        .then(function (wholeArray) {
+        expect(wholeArray)
+            .toContainEqual(["John", "May", "Elon", "Chris", "Nick"]);
+    })
+        .catch(function (unexpected) { return console.log("Covered"); });
+});
+test('Check if conversion works for arrays', function () {
+    var one = (0, index_1.toPromise)(["John", "Elton"]);
+    var two = (0, index_1.toPromise)(["May"]);
+    var three = (0, index_1.toPromise)(["Elon"]);
+    var four = (0, index_1.toPromise)(["Chris", "West"]);
+    var five = (0, index_1.toPromise)(["Nick"]);
+    var testingArray = [
+        one, two, three,
+        four, five
+    ];
+    (0, index_1.ArrayOfPromisesToPromiseArray)(testingArray)
+        .then(function (wholeArray) {
+        expect(wholeArray)
+            .toContainEqual([["John", "Elton"], ["May"], ["Elon"], ["Chris", "West"], ["Nick"]]);
     })
         .catch(function (unexpected) { return console.log("Covered"); });
 });
--- index.test.ts
+++ index.test.ts
@@ -14,13 +14,54 @@ test('Check if conversion works as expected', () => {
 
     ArrayOfPromisesToPromiseArray(testingArray)
         .then(wholeArray => {
-            expect(wholeArray).toContainEqual([1, 2, 3, 4, 5])
+            expect(wholeArray)
+                .toContainEqual([1, 2, 3, 4, 5])
+        })
+        .catch(unexpected => console.log("Covered"))
+});
+
+test('Check if conversion works for strings', () => {
+    const one = toPromise("John")
+    const two = toPromise("May")
+    const three = toPromise("Elon")
+    const four = toPromise("Chris")
+    const five = toPromise("Nick")
+
+    const testingArray = [
+        one, two, three,
+        four, five
+    ]
+
+    ArrayOfPromisesToPromiseArray(testingArray)
+        .then(wholeArray => {
+            expect(wholeArray)
+                .toContainEqual(["John", "May", "Elon", "Chris", "Nick"])
+        })
+        .catch(unexpected => console.log("Covered"))
+});
+
+test('Check if conversion works for arrays', () => {
+    const one = toPromise(["John", "Elton"])
+    const two = toPromise(["May"])
+    const three = toPromise(["Elon"])
+    const four = toPromise(["Chris", "West"])
+    const five = toPromise(["Nick"])
+
+    const testingArray = [
+        one, two, three,
+        four, five
+    ]
+
+    ArrayOfPromisesToPromiseArray(testingArray)
+        .then(wholeArray => {
+            expect(wholeArray)
+                .toContainEqual([["John", "Elton"], ["May"], ["Elon"], ["Chris", "West"], ["Nick"]])
         })
         .catch(unexpected => console.log("Covered"))
 });
 
 test('Correctly handles empty array', () => {
-    const testingArray: Array<Promise<string>> =[]
+    const testingArray: Array<Promise<string>> = []
     ArrayOfPromisesToPromiseArray(testingArray)
         .then(wholeArray => {
             expect(wholeArray).toContainEqual([])
